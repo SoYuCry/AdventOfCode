af76efbdcc1e0990667ecba00f7863472df15ddf
Liuc <liucsjtu@gmail.com>
2025-12-14 16:34:32 +0800

    Day4_part2

--- Day4.py
+++ Day4.py
@@ -6,6 +6,12 @@ eight_direction = [(-1,0),(-1,-1),(-1,1),(0,1),(0,-1),(1,0),(1,-1),(1,1)]
 # 1. 没有判断，(i,j) 的位置是否是 @
 # 2. 没有对 C < 0 做判断，会导致取 line[-1]
 
+# part2 犯的错误
+# 1. read 可以优化，从 lines = f.read().strip().split("\n") 改为 lines = [list(line.rstrip('\n')) for line in f]
+#    这样做的好处是，可以通过 lines[i][j] = '.' 赋值
+# 2. 理解错题意，本来以为是 remove 一个就 update 一次，但是其实是根据快照全部 remove 掉才进行下一个 round
+# 3. while 循环一开始有点点懵，还有 removeables = [] 的重置位置应该在 remove 动作之后，差点搞错。以及 for x in xs 是允许 xs 为空的
+
 def solve_part1(three_lines,i):
 
     # the line uppper in the (i,j)
@@ -27,34 +33,42 @@ def solve_part1(three_lines,i):
             return False
     return True
 
-# def contruct_three_line()
-
 
 def main():
     # 读入 AoC Day 3 的那一行输入
     with open('./input_day4.txt', 'r') as f:
-        lines = f.read().strip().split("\n")
+        lines = [list(line.rstrip('\n')) for line in f]
     
     result = 0
-    for j in range(0,len(lines)):
-        if j-1 >= 0:
-            line1 = lines[j-1]
-        else:
-            line1 = "."*len(lines[j])
-        line2 = lines[j]
-        if j+1 >= len(lines):
-            line3 = '.'*len(lines[j])
-        else:
-            line3 = lines[j+1]
-        
-        three_lines = [line1,line2,line3]
-        for i in range(0,len(lines[j])):
+    removeables = []
+    while True:
+        for removeable in removeables:
+            lines[removeable[0]][removeable[1]] = '.'
+        removeables = []
+        for j in range(0,len(lines)):
+            if j-1 >= 0:
+                line1 = lines[j-1]
+            else:
+                line1 = "."*len(lines[j])
+            line2 = lines[j]
+            if j+1 >= len(lines):
+                line3 = '.'*len(lines[j])
+            else:
+                line3 = lines[j+1]
+            
+            three_lines = [line1,line2,line3]
+            for i in range(0,len(lines[j])):
 
-            if lines[j][i] != '@':
-                continue
+                if lines[j][i] != '@':
+                    continue
 
-            if solve_part1(three_lines,i):
-                result += 1
+                if solve_part1(three_lines,i):
+                    # if removeable, add into stroe, update all lines and solve again
+                    result += 1
+                    removeables.append([j,i])
+
+        if len(removeables) == 0:
+            break
     
     print(result)
 
